15537880c3ee1d5e890a3ed8af69aa0eecc00bb9
renukachhabdiya <renu.chhabdiya@gmail.com>
2016-07-03 23:36:44 -0700

    Create HEADER.PHP
    HEADER

---
+++ HEADER.PHP
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"https://github.com/renukachhabdiya0000/rtcamp123.git>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" type="text/css" rel="stylesheet" />
+<title><?php echo $title ?></title>
+<script type="text/javascript" src="script.js"></script>
+</head>
+
+<body style="background:url('rt.jpg') repeat">
+<center>
+<div id="container" style="width:1000px">
+<marquee><div style="color:black; font-size:50px; letter-spacing:2px; text-shadow: 0 0 0.1em rgba(51,204,0,0.6);">
+TWITTER
+</div></marquee>
+<hr /><br />
+<?php include_once 'navigation.php' ?>
+<br><br>
+<h2><?php echo $heading ?></h2>
+<hr width="50%" />
