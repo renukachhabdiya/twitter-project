d4761a90db3e60caa74e1f335f15605eaceb6aba
renukachhabdiya <renu.chhabdiya@gmail.com>
2016-07-03 23:30:42 -0700

    Create Login.php
    login page

---
+++ Login.php
@@ -0,0 +1,31 @@
+<html>
+
+	<head>
+		<title>Twitter</title>
+	</head>
+	<body>
+		
+		<center><font size="6" face="Verdana"><b>TWITTER WORLD</b></font></center>
+		<marquee><font size="5" face="Arial"><b>Find everything here </b></font></marquee>
+		<center>
+		<img src="rt.jpg"  height="150px" width="150px" />
+		
+	
+		<h2>LOGIN </h2>
+		
+		<form action="checkuser.php" method ="post">
+		<table>
+			<tr><td>Username</td><td><input type="text" name="user"></td></tr>
+			<tr><td>Password</td><td><input type="password" name="pwd"></td></tr>
+			
+                       
+			 <tr align="center"><td colspan="2"><input type="submit" value="SUBMIT"></td></tr>
+		</table>
+		</form>
+		</center>
+		<center><h2> JOIN OUR WORLD </h2><center>
+		<center> <a color="red" href='registform.php'>Click here for creating account</a></center>
+		
+	</body>
+
+</html>
